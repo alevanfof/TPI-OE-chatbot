ef2b347a369173179d7b25b4df700d659c8c9e1e
Alejandro <afernandez@iset55.edu.ar>
2026-06-04 14:02:56 -0300

    [PROY-1] Implementa validacion de empleado y control inicial de flujo

--- chatbot_vacaciones.py
+++ chatbot_vacaciones.py
@@ -1,34 +1,57 @@
-# --- FLUJO PRINCIPAL Conversacional (MÁQUINA DE ESTADOS) ---
-
 def chatbot_principal():
     mostrar_cabecera()
 
     empleados = cargar_empleados()
 
-    bot_imprimir("👋 ¡Hola! Soy el asistente virtual de Recursos Humanos de la empresa.")
-    bot_imprimir("Estoy acá para ayudarte a registrar tu **Solicitud de Vacaciones** de forma digital.")
-
     while True:
-
-        # --- ESTADO 1: INICIO DEL PROCESO ---
-        bot_imprimir("¿Querés registrar una nueva solicitud de vacaciones? (si / /ayuda):")
 
         opcion = input(f"{COLOR_USER}[Vos]: {COLOR_RESET}").strip()
 
-        if opcion.lower() == "/ayuda":
-            mostrar_ayuda()
-            continue
-
-        elif opcion.lower() == "/empleados":
-            mostrar_empleados_consola(empleados)
-            continue
-
-        elif opcion.lower() == "/cancelar":
-            bot_imprimir("No hay ninguna solicitud activa para cancelar actualmente.")
-            continue
-
         if opcion.lower() not in ["si", "s", "sí", "iniciar"]:
-            bot_imprimir("Entendido. Quedo a tu disposición.")
             continue
 
         solicitud_activa = True
+
+        id_empleado = ""
+        nombre_empleado = ""
+        dias_disponibles = 0
+
+        while solicitud_activa:
+
+            bot_imprimir("Ingresá tu ID de empleado (ej: EMP002) o '/cancelar':")
+
+            id_input = input(f"{COLOR_USER}[Vos]: {COLOR_RESET}").strip().upper()
+
+            if id_input == "/CANCELAR":
+                bot_imprimir("❌ Solicitud abortada por el usuario.")
+                solicitud_activa = False
+                break
+
+            elif id_input == "/AYUDA":
+                mostrar_ayuda()
+                continue
+
+            elif id_input == "/EMPLEADOS":
+                mostrar_empleados_consola(empleados)
+                continue
+
+            sys_imprimir(f"Validando ID '{id_input}' en empleados.txt...")
+
+            if id_input in empleados:
+
+                id_empleado = id_input
+                nombre_empleado = empleados[id_input]["nombre"]
+                dias_disponibles = empleados[id_input]["disponibles"]
+
+                bot_imprimir(f"Empleado: {nombre_empleado}")
+                bot_imprimir(f"Saldo disponible: {dias_disponibles} días")
+
+                if dias_disponibles <= 0:
+                    bot_imprimir("⚠️ Saldo insuficiente.")
+                    solicitud_activa = False
+                    break
+
+                break
+
+            else:
+                bot_imprimir("❌ Error: ID no encontrado.")
